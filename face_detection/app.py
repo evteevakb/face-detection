5ad97f088d6d817156bc52b0d412a41f24f3217a
Ksenia Evteeva <k.evteeva@skoltech.ru>
2023-03-06 00:36:03 +0300

    Add API

--- face_detection/app.py
+++ face_detection/app.py
@@ -1,10 +1,19 @@
+import io
+from enum import Enum
+import logging
 from pathlib import Path
-from typing import Union
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.responses import Response
 from PIL import ImageDraw
 
 from utils import FaceAPI, MinioClient, Mongo
+
+
+class Colors(str, Enum):
+    red = "red"
+    green = "green"
+    blue = "blue"
 
 
 app = FastAPI()
@@ -13,24 +22,47 @@ minio = MinioClient()
 mongo = Mongo()
 
 
+# setup logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+screen_logs = logging.StreamHandler()
+file_logs = logging.handlers.RotatingFileHandler("app.log", mode="a",
+                                                 maxBytes = 100*1024, backupCount = 3)
+formatter = logging.Formatter(
+    "%(asctime)s - %(module)s - %(funcName)s - line:%(lineno)d - %(levelname)s - %(message)s")
+screen_logs.setFormatter(formatter)
+file_logs.setFormatter(formatter)
+logger.addHandler(screen_logs)
+logger.addHandler(file_logs)
+
+
+@app.get("/")
+async def root() -> dict:
+    return {"message": "Welcome to face detection application based on Face++ and FastAPI"}
+
+
 @app.post("/image")
-def detect(local_path: Union[str, Path]) -> dict:
-    if isinstance(local_path, str):
-        local_path = Path(local_path)
-    result = face_api.detect(local_path)
+async def detect(file: UploadFile = File(...)) -> dict:
+    contents = await file.read()
+    result = face_api.detect(contents)
     if "error_message" in result:
         raise HTTPException(status_code=result["status_code"], detail=result["error_message"])
     if len(result["faces"]) == 0:
-        raise HTTPException(status_code=404, detail=f"Faces on the image {local_path} not found")
+        raise HTTPException(status_code=404, detail="Faces on the image are not found")
     image_id = result["image_id"]
-    image_name = minio.load_object(local_path, image_id)
+    try:
+        image_name = minio.load_object(contents, image_id)
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail="Could not connect to MinIO storage") from exc
     mongo.post_document(result, image_name)
     return {"id": image_id}
 
 
-@app.get("/image/<id>?color=<red/green/blue>")
-def get(image_id, color):
+@app.get("/image/{image_id}", response_class=Response)
+async def get(image_id: str, color: Colors) -> Response:
     document = mongo.get_document(image_id)
+    if document is None:
+        raise HTTPException(status_code=404, detail=f"Image with image_id={image_id} not found")
     image = minio.get_object(document["image_name"])
     draw = ImageDraw.Draw(image)
     for face in document["faces"]:
@@ -38,21 +70,29 @@ def get(image_id, color):
         draw.rectangle([bbox["left"], bbox["top"],
                         bbox["left"]+bbox["width"], bbox["top"]+bbox["height"]],
                        fill=None, outline=color, width=1)
-    return image
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, format='PNG')
+    image_bytes = image_bytes.getvalue()
+    return Response(content=image_bytes, media_type="image/png")
 
 
-@app.put("/image/<id>")
-def put(local_path, image_id):
-    if isinstance(local_path, str):
-        local_path = Path(local_path)
-    result = face_api.detect(local_path)
-    document = mongo.update_document(image_id, result)
-    minio.replace_object(local_path, document["image_name"], document["image_id"])
+@app.put("/image/{image_id}")
+async def put(image_id: str, file: UploadFile = File(...)):
+    contents = await file.read()
+    result = face_api.detect(contents)
+    if "error_message" in result:
+        raise HTTPException(status_code=result["status_code"], detail=result["error_message"])
+    if len(result["faces"]) == 0:
+        raise HTTPException(status_code=404, detail="Faces on the image are not found")
+    _ = mongo.update_document(image_id, result)
+    minio.replace_object(contents, image_id)
+    return {"id": image_id}
 
 
-@app.delete("/image/<id>")
-def remove(image_id):
+@app.delete("/image/{image_id}")
+def remove(image_id: str):
     document = mongo.get_document(image_id)
     object_name = document["image_name"]
     minio.remove_object(object_name)
     mongo.remove_document(object_name)
+    return {"id": image_id}
--- face_detection/utils.py
+++ face_detection/utils.py
@@ -11,6 +11,8 @@ from pymongo import MongoClient
 from pymongo.collection import ReturnDocument
 import requests
 
+from fastapi import HTTPException
+
 
 class FaceAPI:
     def __init__(self):
@@ -18,47 +20,42 @@ class FaceAPI:
         self.face_key = os.environ["FACE_KEY"]
         self.face_secret = os.environ["FACE_SECRET"]
 
-    def detect(self, local_path: Path, timeout: int = 600):
-        with open(local_path, "rb") as image:
-            image64 = base64.b64encode(image.read())
-        result = requests.post(self.face_api, data={"api_key": self.face_key,
+    def detect(self, image: bytes, timeout: int = 600) -> dict:
+        image64 = base64.b64encode(image)
+        response = requests.post(self.face_api, data={"api_key": self.face_key,
                                                     "api_secret": self.face_secret,
                                                     "image_base64": image64},
                                timeout=timeout)
-        if result.status_code != 200:
-            result["status_code"] = result.status_code
-        return result.json()
+        result = response.json()
+        if response.status_code != 200:
+            result["status_code"] = response.status_code
+        return result
 
 
 class MinioClient:
     def __init__(self):
         self.bucket = os.environ["INITIAL_BUCKET_NAME"]
-        self.minio = Minio("localhost:9000", access_key=os.environ["MINIO_ROOT_USER"],
-                           secret_key=os.environ["MINIO_ROOT_PASSWORD"], secure=True)
+        self.minio = Minio("minio:9000", access_key=os.environ["MINIO_ROOT_USER"],
+                           secret_key=os.environ["MINIO_ROOT_PASSWORD"], secure=False)
 
-    def load_object(self, local_path: Union[str, Path], image_id: int):
-        if isinstance(local_path, str):
-            local_path = Path(local_path)
-        image_name = f"{image_id}{local_path.suffix}"
-        result = self.minio.fput_object(self.bucket, image_name, local_path)
+    def load_object(self, content: bytes, image_id: str):
+        content = BytesIO(content)
+        result = self.minio.put_object(bucket_name=self.bucket, object_name=image_id,
+                                       data=content, length=content.getbuffer().nbytes)
         return result.object_name
 
     def get_object(self, object_name: str):
-        try:
-            response = self.minio.get_object(self.bucket, object_name)
-            image = Image.open(BytesIO(response.read()))
-            return image
-        finally:
-            response.close()
-            response.release_conn()
+        response = self.minio.get_object(self.bucket, object_name)
+        image = Image.open(BytesIO(response.read()))
+        return image
 
-    def remove_object(self, object_name: str):
-        self.minio.remove_object(self.bucket, object_name)
+    def remove_object(self, image_id: str):
+        self.minio.remove_object(self.bucket, image_id)
 
 
-    def replace_object(self, local_path, object_name, image_id):
-        self.remove_object(object_name)
-        object_name = self.load_object(local_path, image_id)
+    def replace_object(self, content: bytes, image_id: str):
+        self.remove_object(image_id)
+        object_name = self.load_object(content, image_id)
         return object_name
 
 
@@ -76,21 +73,20 @@ class Mongo:
                     "image_id": result["image_id"],
                     "time_used": result["time_used"],
                     "date": datetime.datetime.utcnow()}
-        post_id = self.collection.insert_one(document).inserted_id
-        return post_id
+        if self.collection.count_documents({'image_id': result["image_id"]}, limit=1) == 0:
+            self.collection.insert_one(document)
 
     def remove_document(self, image_id: str):
         self.collection.delete_one({'image_id': image_id})
 
     def get_document(self, image_id: str):
-        document = self.collection.find_one({'image_id': image_id})
-        return document
+        return self.collection.find_one({'image_id': image_id})
 
     def update_document(self, image_id, result):
         document = {"request_id": result["request_id"],
                     "faces": result["faces"],
                     "time_used": result["time_used"],
                     "date": datetime.datetime.utcnow()}
-        document = self.collection.find_one_and_update({"image_id": image_id}, {'$set': document},
+        document = self.collection.find_one_and_update({"image_id": image_id}, {"$set": document},
                                                        return_document=ReturnDocument.AFTER)
         return document
